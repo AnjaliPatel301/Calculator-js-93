a6868f87a01582069b8a15acbc6b348e315f9fa7
Anjali <sj5065070@gmail.com>
2025-05-28 07:36:23 +0530

    This is new files

---
+++ README.md
@@ -0,0 +1,26 @@
+# 🧮 Simple Calculator
+
+A basic **Calculator web app** built using **HTML, CSS, and JavaScript**. It performs standard arithmetic operations like addition, subtraction, multiplication, and division. A clean UI with responsive design makes it user-friendly on all devices.
+
+---
+
+## 🚀 Features
+
+- Basic arithmetic operations: ➕ ➖ ✖️ ➗  
+- Responsive design for mobile and desktop  
+- Clear and Delete buttons  
+- Keyboard input support (optional)  
+- Built with only vanilla JavaScript (no libraries/frameworks)
+
+---
+
+## 🛠️ Technologies Used
+
+- **HTML5** – Structure of the calculator
+- **CSS3** – Styling and layout
+- **JavaScript ** – Logic and functionality
+
+
+
+<!
+
---
+++ script.js
@@ -0,0 +1,113 @@
+var curvalue='';
+  
+
+const a=document.querySelector("#a").addEventListener('click',()=>{
+ curvalue=curvalue+'7'
+ document.querySelector('.display').value=curvalue;
+
+})
+//console.log(a)
+
+
+const b=document.querySelector("#b").addEventListener('click',()=>{
+    curvalue=curvalue+'8'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+  // console.log(c)
+
+   
+  const c=document.querySelector("#c").addEventListener('click',()=>{
+    curvalue=curvalue+'9'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+  // console.log(a) 
+const d=document.querySelector("#d").addEventListener('click',()=>{
+    curvalue=curvalue+'+'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+  // console.log(e)
+ const e=document.querySelector("#e").addEventListener('click',()=>{
+    curvalue=curvalue+'4'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+   //console.log(a)
+   const f=document.querySelector("#f").addEventListener('click',()=>{
+    curvalue=curvalue+'5'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+   //console.log(a)
+   const g=document.querySelector("#g").addEventListener('click',()=>{
+    curvalue=curvalue+'6'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+  // console.log(a)
+
+  const h=document.querySelector("#h").addEventListener('click',()=>{
+    curvalue=curvalue+'-'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+
+   const i=document.querySelector("#i").addEventListener('click',()=>{
+    curvalue=curvalue+'1'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+   //console.log(a)
+   const j=document.querySelector("#j").addEventListener('click',()=>{
+    curvalue=curvalue+'2'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+   //console.log(a)
+   const k=document.querySelector("#k").addEventListener('click',()=>{
+    curvalue=curvalue+'3'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+  // console.log(a)
+
+  const l=document.querySelector("#l").addEventListener('click',()=>{
+    curvalue=curvalue+'*'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+   //console.log(a)
+
+
+
+   const m=document.querySelector("#m").addEventListener('click',()=>{
+    curvalue=''
+       document.querySelector('.display').value=curvalue;
+   
+   })
+   //console.log(a)
+   const n=document.querySelector("#n").addEventListener('click',()=>{
+    curvalue=curvalue+'0'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+   //console.log(a)
+   const o=document.querySelector("#o").addEventListener('click',()=>{
+    curvalue=eval(curvalue)
+    document.querySelector('.display').value=curvalue;
+   
+   })
+  // console.log(a)
+
+  const p=document.querySelector("#p").addEventListener('click',()=>{
+    curvalue=curvalue+'/'
+    document.querySelector('.display').value=curvalue;
+   
+   })
+
+
+
+
+
